0dc6696f71d8d2c9ad69bf8e0fc62f35aa6a7a11
amina-islam-meem <amina.meem28@gmail.com>
2026-01-02 21:56:06 +0600

    cf code

---
+++ Suspension.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define nl '\n'
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+ 
+    int T; cin >>  T;
+    while(T--)
+    {
+        int n; cin >> n;
+
+        
+    }
+    return 0;
+}
