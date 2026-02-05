6d0e9e007f7621674ccffc16994845513a9fd41d
amina-islam-meem <amina.meem28@gmail.com>
2026-02-06 00:35:35 +0600

    Place of the Olympiad

---
+++ Place of the Olympiad.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define nl '\n'
+
+
+bool check(ll mid,ll n,ll m,ll k)
+{
+    ll p = m /(mid + 1);
+    ll r = m % (mid + 1);
+
+    ll count = p* mid+ min(mid, r);
+
+    return count >= (k + n - 1) / n;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int T; cin >>  T;
+    while(T--)
+    {
+        ll n,m,k; cin >> n >> m >> k;
+
+
+
+        ll l = 0,r = m;
+
+        while(l+1 < r)
+        {
+            ll mid = l + (r-l)/2;
+
+            if(check(mid,n,m,k))
+                r= mid;
+            else
+                l = mid;
+        }
+
+        cout << r << nl;
+
+
+    }
+    return 0;
+}
+
